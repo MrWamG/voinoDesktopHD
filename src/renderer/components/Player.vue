31436d530b908769544888281a0b1d7cd4cb3bfc
MrWamG <wangliamgcom@gmail.com>
2022-04-29 17:22:28 +0800

    初始化

---
+++ src/renderer/components/Player.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="player">
+        <div class="test">
+            <div class="child">test</div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Player',
+    components: {},
+    data () {
+      return {}
+    },
+    created () { },
+    unmounted () { },
+    methods: {}
+  }
+</script>
+
+<style>
+    .test{
+        /* .child{
+            width: 50px;
+            height: 50px;
+            background: #000;
+        } */
+    }
+</style>
